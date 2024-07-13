42640fb2f410206f3e858ec35cf2c63ece2bed37
sanketnighot <sanketnighot25@gmail.com>
2024-07-13 13:09:56 +0530

    added an log message in pre-commit file

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -67,6 +67,7 @@ userSchema.pre("save", async function (next) {
 })
 
 userSchema.methods.isPasswordCorrect = async function (password) {
+  console.log(password)
   return await bcrypt.compare(password, this.password)
 }
 
