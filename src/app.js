3be0aa8b1d453d9ff505219f8acb4308d3aa13cc
sanketnighot <sanketnighot25@gmail.com>
2024-07-14 03:20:20 +0530

    fix: fixing bugs in the handler code

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
+import ApiError from "./utils/ApiError.js"
 
 const app = express()
 
@@ -9,5 +10,37 @@ app.use(express.json({ limit: "16kb" }))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+// Routes Import
+import userRouter from "./routes/user.routes.js"
+
+// Routes Declaration
+app.use("/api/v1/users", userRouter)
+app.get("/", (req, res) => {
+  try {
+    res.status(200).json({ message: "Connected to Therapix API" })
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
+})
+
+app.use((err, req, res, next) => {
+  if (err instanceof ApiError) {
+    res.status(err.statusCode).json({
+      statusCode: err.statusCode,
+      success: err.success,
+      message: err.message,
+      errors: err.errors,
+      stack: err.stack,
+    })
+  } else {
+    res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    })
+  }
+
+  next()
+})
 
 export default app
--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 8000
 connectDB()
   .then(() => {
     app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`)
+      console.log(`⚙️ Server is running on port ${PORT}`)
     })
   })
-  .catch((err) => console.log("MONGODB Connection Failed !!! ", err))
+  .catch((err) => console.log("❌ MONGODB Connection Failed !!! ", err))
